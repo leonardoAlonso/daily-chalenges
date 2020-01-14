fdfe9c41c9bb00d722f029cdb3e27d09c98e8c5b
Leonardo Alonso <lalonsopc@corporativosade.com.mx>
2020-01-14 10:10:00 -0600

    python isograma

---
+++ python/isograma.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Determine if a word is an isogram
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+
+https://dev.to/thepracticaldev/daily-challenge-159-isogram-3h4
+
+"""
+
+def is_isogram(word):
+    word = word.lower()
+    return len(set(word)) == len(word)
+
+assert is_isogram("Dermatoglyphics") == True
+assert is_isogram("aba") == False
